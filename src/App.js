d048ca60a0c35d396d95708e0ef268f5eebb8464
Pavan Mirla <pavanmirla@gmail.com>
2021-01-17 15:57:46 -0800

    Local storage is not reqired But useState with hardcoded count data is required to work with refresh

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import "./styles.css";
 
 export default function App() {
   const [count, setCount] = React.useState(0);
-  React.useEffect(() => {
-    localStorage.setItem("count", count);
-  }, [count]);
-
-  React.useEffect(() => {
-    const parsedCount = Number(localStorage.getItem("count") || 0);
-    setCount(parsedCount);
-  }, []);
-
   return (
     <>
       <div className="App">Hello</div>
